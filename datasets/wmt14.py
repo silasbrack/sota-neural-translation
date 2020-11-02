58c367366325ae2ca23eddfd6afee8011a1f1932
silasbrack <s174433@student.dtu.dk>
2020-11-02 21:08:09 +0100

    Implemented simple WMT 2014 data loader.

--- datasets/wmt14.py
+++ datasets/wmt14.py
@@ -1,8 +1,18 @@
 
 from datasets import load_dataset
-# from torch.utils.data import Dataset
 
-def load(lang='fr-en'):
-    dataset = load_dataset('wmt14', lang)
+def load(lang, type=None):
+    """
+    The first time you load a dataset it will take a while to download all of the files.
+    It should then save it to the __pycache__ folder and then load quickly again.
 
-# train, val, test = WMT14.splits(exts=('.de', '.en'), fields=(DE, EN))
+    Parameters
+    ----------
+    lang : str
+        Which languages to load in ['cs-en', 'de-en', 'fr-en', 'hi-en', 'ru-en']
+    type : str
+        Output type selected in [None, ‘numpy’, ‘torch’, ‘tensorflow’, ‘pandas’] None means __getitem__ returns python objects (default)
+    """
+    dataset = load_dataset('wmt14', lang).set_format(type)
+    return dataset
+
